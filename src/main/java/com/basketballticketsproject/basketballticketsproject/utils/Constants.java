641f883af01c95b65895c619aef7e67c19aa828f
evatf01 <evatf01@gmail.com>
2023-11-22 15:38:24 +0100

    añadir delete y modificar usuario

---
+++ src/main/java/com/basketballticketsproject/basketballticketsproject/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.basketballticketsproject.basketballticketsproject.utils;
+
+public class Constants {
+
+    public static int NUM_ENTRADAS = 50;
+
+    public static String PATH_CARPETA_FECHAS_PARTIDOS = "C:\\Fechas_Partidos";
+}
